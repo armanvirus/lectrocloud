ff3c59facf8184c7cbc0b4ca2169ae6c748a7032
Arman Grema <abdurrahmangrema19@gmail.com>
2023-01-05 13:01:57 -1100

    added image pre-upload image preview

--- client/src/pages/AddPost.jsx
+++ client/src/pages/AddPost.jsx
@@ -1,8 +1,25 @@
-import React from 'react'
+import React,{useState} from 'react'
 import amin from "../assets/amin.png"
 import "../styles/addpost.css"
 
 export default function AddPost() {
+    const [selectedFile, setselectedFile] = useState("");
+    const [previewUrl, setpreviewUrl] = useState('');
+
+    const inputChange = (e)=>{
+        const file = e.target.files[0]
+        console.log(file)
+        previewFile(file);
+    }
+
+    const previewFile = (file)=>{
+        const reader = new FileReader();
+        reader.readAsDataURL(file);
+        reader.onloadend = ()=>{
+            console.log(reader.result)
+            setpreviewUrl(reader.result)
+        }
+    }
     return (
         <div>
             <div className="light-container">
@@ -14,7 +31,7 @@ export default function AddPost() {
             <div className="files">
                 <div>
                     <label htmlFor="image">
-                    <input type="file" name="image" id="image"/>
+                    <input onChange={(e)=> inputChange(e) } type="file" name="image" id="image"/>
                     <spna className="material-symbols-outlined">landscape</spna>
                     </label>
                </div>
@@ -25,6 +42,11 @@ export default function AddPost() {
                     </label>
                </div>
             </div>
+            {
+                previewUrl && (
+                    <img className="preview-image" src={previewUrl} alt="something is wrong"/>
+                )
+            }
             <div className="send-btn">
             <button> <span> light </span> <span className="material-symbols-outlined">bolt</span> </button>
             </div>
