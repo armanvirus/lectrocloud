09fbc1c77bdccde2365c9340231f7aa9d7b3f9b4
Arman Grema <abdurrahmangrema19@gmail.com>
2023-01-12 13:54:29 +0100

    Create commentsController.js

---
+++ server/controllers/commentsController.js
@@ -0,0 +1,11 @@
+const jwt = require("jsonwebtoken")
+const lights = require("../db/lights")
+module.exports ={
+  comments:(req,res)=>{
+    const {comment,ightId, user, images, files} = req.body;
+    if(user){
+      
+      }
+    }
+  
+  }
