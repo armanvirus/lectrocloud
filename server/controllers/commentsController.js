f3d2d2e1b9417df3db75c964a39417fbcbe7bd45
Arman Grema <abdurrahmangrema19@gmail.com>
2023-01-12 14:14:17 +0100

    Update commentsController.js

--- server/controllers/commentsController.js
+++ server/controllers/commentsController.js
@@ -1,9 +1,26 @@
 const jwt = require("jsonwebtoken")
-const lights = require("../db/lights")
+const lights = require("../db/lights");
+const commentdb = require("../db/comments");
 module.exports ={
   comments:(req,res)=>{
     const {comment,ightId, user, images, files} = req.body;
     if(user){
+      jwt.verify(user, "JWT_SECRET", (error, decoded) =>{
+        if(error) throw error;
+        const newComment = new commentdb({
+          comment,
+          mainLight:lightId,
+          user:{
+            id:decoded.id,
+            name:decoded.name,
+            profile:decoded.profile
+          },
+          
+          
+        })
+     
+        })
+      
       
       }
     }
