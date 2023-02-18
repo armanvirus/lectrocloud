9ea88151d3c577113b58b13f360d7c080ed41594
Arman Grema <abdurrahmangrema19@gmail.com>
2023-02-18 04:39:36 -1100

    relaced hard coded server url with env var

--- client/src/pages/signup/Contact.jsx
+++ client/src/pages/signup/Contact.jsx
@@ -1,6 +1,7 @@
 import React,{useState,useEffect} from 'react'
 import axios from 'axios'
 import {useNavigate} from "react-router-dom"
+import {serverUrl} from "../../utils/Datum"
 
 export default function Contact(props) {
     const [phone, setphone] = useState("");
